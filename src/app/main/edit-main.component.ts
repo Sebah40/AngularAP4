47372e6934b7ca5a2c4c4192e4f2a00b801a5d2d
Sebah40 <106514501+Sebah40@users.noreply.github.com>
2023-05-28 18:38:20 -0300

    Finished adding URL for image upload

--- src/app/main/edit-main.component.ts
+++ src/app/main/edit-main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { persona } from '../model/persona.model';
 import { PersonaService } from '../service/persona.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ImageService } from '../service/image.service';
 
 @Component({
   selector: 'app-edit-main',
@@ -11,7 +12,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class EditMainComponent implements OnInit {
   person: persona = null;
 
-  constructor(private personaS: PersonaService, private activatedRouter: ActivatedRoute, private router: Router) {}
+  constructor(private personaS: PersonaService, private activatedRouter: ActivatedRoute, private router: Router, public imageService: ImageService) {}
   
   ngOnInit(): void {
     const id = this.activatedRouter.snapshot.params['id'];
@@ -24,7 +25,11 @@ export class EditMainComponent implements OnInit {
     )
   }
 
-
+  uploadImage($event: any) {
+    const id = this.activatedRouter.snapshot.params['id'];
+    const name = "profile_" + id;
+    this.imageService.uploadImage($event, name);
+  }
 
   onUpdate(): void {
     const id = this.activatedRouter.snapshot.params['id'];
